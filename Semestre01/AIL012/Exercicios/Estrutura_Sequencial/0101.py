c4ebb7e16eed9056c77d038df731a932b72fda3a
Marcos Assuncao <dc.massuncao@gmail.com>
2025-02-19 19:41:58 -0300

    Create 0101.py

---
+++ Semestre01/AIL012/Exercicios/Estrutura_Sequencial/0101.py
@@ -0,0 +1,10 @@
+
+print('Digite o primeiro numero')
+primeiro = int(input('>_ '))
+
+print('Digite o segundo numero')
+segundo = int(input('>_ '))
+
+subtracao =  primeiro - segundo
+
+print(f'A subtracao de {primeiro} - {segundo} = {subtracao}')
